6bb3a10327cbe0d638da6adc60acfe8761b41436
David H. Friedman <david.h.friedman.nyc@gmail.com>
2020-01-18 06:21:38 -0500

    Part 3 Web Forms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -34,3 +35,13 @@ def index():
     user = 8
     ps = [p for p in posts if p['author'] == user]
     return render_template('index.html', title='series 1', user=user, users=users, posts=ps)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    dir(form)
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
